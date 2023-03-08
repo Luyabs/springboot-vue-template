10fb801c628cbb89cc448fc31fd30fcead317f68
Lu Yang <74538732+Luyabs@users.noreply.github.com>
2023-03-08 23:38:19 +0800

    Update BookServiceImpl.java

--- springboot-advanced-template/src/main/java/com/example/abs/service/impl/BookServiceImpl.java
+++ springboot-advanced-template/src/main/java/com/example/abs/service/impl/BookServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.abs.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.abs.entity.Book;
 import com.example.abs.mapper.BookMapper;
@@ -12,4 +15,13 @@ import org.springframework.stereotype.Service;
 public class BookServiceImpl extends ServiceImpl<BookMapper, Book> implements BookService {
     @Autowired
     private BookMapper bookMapper;
+
+    @Override
+    public IPage<Book> selectPage(int currentPage, int pageSize, Book book) {
+        QueryWrapper<Book> wrapper = new QueryWrapper<Book>()
+                .like(book.getType() != null, "type", book.getType())
+                .like(book.getName() != null, "name", book.getName())
+                .like(book.getDescription() != null, "description", book.getDescription());
+        return bookMapper.selectPage(new Page<>(currentPage, pageSize), wrapper);
+    }
 }
