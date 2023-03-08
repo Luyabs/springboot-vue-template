63832cb2726f02f179f787cd05597a76fc8ede34
Lu Yang <74538732+Luyabs@users.noreply.github.com>
2023-03-08 23:38:04 +0800

    Update BookService.java

--- springboot-advanced-template/src/main/java/com/example/abs/service/BookService.java
+++ springboot-advanced-template/src/main/java/com/example/abs/service/BookService.java
@@ -1,8 +1,10 @@
 package com.example.abs.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.abs.entity.Book;
 
 public interface BookService extends IService<Book> {
 
+    IPage<Book> selectPage(int currentPage, int pageSize, Book book);
 }
